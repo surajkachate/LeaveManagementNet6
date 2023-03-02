0b636595276c265206cbf595f54c50692940331d
Suraj Kachate <surajkachate021@gmail.com>
2023-03-02 15:13:25 +0530

    Update datatables modify edit, details, delete buttons

---
+++ LeaveManagement.Web/Configurations/MapperConfg.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurations
+{
+    public class MapperConfg : Profile
+    {
+        public MapperConfg() { 
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
